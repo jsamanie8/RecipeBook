3478ab7ccd1e48607859d9c8b78bafc4e4a9a07c
Josh Samaniego <josh.samaniego8@gmail.com>
2021-03-28 12:26:16 -0700

    Starting my api project.

---
+++ Recipe.Web.Api/Controllers/OwnerApiController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Recipe.Web.Api.Controllers
+{
+    [Route("api/owners")]
+    [ApiController]
+    public class OwnerApiController : ControllerBase
+    {
+    }
+}
