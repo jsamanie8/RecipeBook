148bee80922e150a7be8e95afe76513bae1713de
Josh Samaniego <josh.samaniego8@gmail.com>
2021-03-28 13:29:26 -0700

    Remove ignore files and updated test api.

--- Recipe.Models/Requests/Owner/OwnerAddRequest.cs
+++ Recipe.Models/Requests/Owner/OwnerAddRequest.cs
@@ -6,5 +6,9 @@ namespace Recipe.Models.Requests.Owner
 {
     public class OwnerAddRequest
     {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
     }
 }
