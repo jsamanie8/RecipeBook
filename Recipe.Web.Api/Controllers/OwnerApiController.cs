4981b07e4ae22b0bcf19940e721e0c462eb7d1bf
Josh Samaniego <josh.samaniego8@gmail.com>
2021-04-01 07:55:35 -0700

    Started configuring services and wiring up connection string/api

--- Recipe.Web.Api/Controllers/OwnerApiController.cs
+++ Recipe.Web.Api/Controllers/OwnerApiController.cs
@@ -9,6 +9,7 @@ using System.Configuration;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
 
 namespace Recipe.Web.Api.Controllers
 {
@@ -16,11 +17,13 @@ namespace Recipe.Web.Api.Controllers
     [ApiController]
     public class OwnerApiController : ControllerBase
     {
-        private string _connection;
+        private readonly string _connection;
 
-        public OwnerApiController()
+        public OwnerApiController(IConfiguration config)
         {
-            
+            //Work on getting connection string from Startup/Dependency Injection...
+            //Check if the connection string shows up here.
+            _connection = config.GetConnectionString("Default");
         }
         [HttpGet, AllowAnonymous]
         public ActionResult<Owner> Get()
