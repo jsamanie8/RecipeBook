30e6e146afb66bb677c9fd3dff77859c99a5116c
Josh Samaniego <josh.samaniego8@gmail.com>
2021-04-05 07:55:03 -0700

    Added interface for services and configured postman to hit endpoint.

---
+++ Recipe.Services/Interfaces/IOwnerService.cs
@@ -0,0 +1,12 @@
+﻿using Recipe.Models.Domain;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Recipe.Services.Interfaces
+{
+    public interface IOwnerService
+    {
+        Owner Get();
+    }
+}
--- Recipe.Web.Api/Controllers/OwnerApiController.cs
+++ Recipe.Web.Api/Controllers/OwnerApiController.cs
@@ -10,17 +10,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Configuration;
+using Recipe.Services.Interfaces;
+using Microsoft.Extensions.Logging;
 
 namespace Recipe.Web.Api.Controllers
 {
     [Route("api/owners")]
     [ApiController]
-    public class OwnerApiController : ControllerBase
+    public class OwnerApiController : BaseApiController
     {
+        private IOwnerService _service = null;
         private readonly string _connection;
 
-        public OwnerApiController(IConfiguration config)
+        public OwnerApiController(IOwnerService service, ILogger<OwnerApiController> logger, IConfiguration config) : base (logger)
         {
+            _service = service;
             //Work on getting connection string from Startup/Dependency Injection...
             //Check if the connection string shows up here.
             _connection = config.GetConnectionString("Default");
@@ -28,7 +32,8 @@ namespace Recipe.Web.Api.Controllers
         [HttpGet, AllowAnonymous]
         public ActionResult<Owner> Get()
         {
-            return null;
+            //return 
+            return Created201();
         }
     }
 }
