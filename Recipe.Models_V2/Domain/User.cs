f0725d13c2672fcbc4faeeb8e9181c0b93b12184
Josh Samaniego <josh.samaniego8@gmail.com>
2021-06-10 18:33:27 -0700

    Added a user model, user add, and user update. Need to work oncreating service and api.

---
+++ Recipe.Models_V2/Domain/User.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Recipe.Models_V2.Domain
+{
+    public class User : Owner
+    {
+        public bool Admin { get; set; }
+    }
+}
---
+++ Recipe.Models_V2/Requests/User/UserUpdateRequest.cs
@@ -0,0 +1,12 @@
+﻿using Recipe.Models_V2.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Recipe.Models_V2.Requests.User
+{
+    public class UserUpdateRequest : UserAddRequest, IModelIdentifier
+    {
+        public int Id { get; set; }
+    }
+}
